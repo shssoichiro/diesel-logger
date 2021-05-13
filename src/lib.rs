ccceb0dd36e7fa821bf854e4755f32fd696a0ecf
fina <code@felinira.net>
2021-05-13 19:38:00 +0200

    Update to diesel master

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,18 @@ extern crate diesel;
 #[macro_use]
 extern crate log;
 
-use std::ops::Deref;
 use std::time::{Duration, Instant};
 
-use diesel::backend::{Backend, UsesAnsiSavepointSyntax};
-use diesel::connection::{AnsiTransactionManager, SimpleConnection};
+use diesel::backend::Backend;
+use diesel::connection::{SimpleConnection, TransactionManager};
 use diesel::debug_query;
-use diesel::deserialize::QueryableByName;
+use diesel::deserialize::FromSqlRow;
+use diesel::expression::QueryMetadata;
 use diesel::prelude::*;
 use diesel::query_builder::{AsQuery, QueryFragment, QueryId};
-use diesel::sql_types::HasSqlType;
+use diesel::query_dsl::CompatibleType;
+use diesel::r2d2::R2D2Connection;
+use std::sync::Arc;
 
 /// Wraps a diesel `Connection` to time and log each query using
 /// the configured logger for the `log` crate.
@@ -20,79 +22,57 @@ use diesel::sql_types::HasSqlType;
 /// an `info` on queries that take longer than 1 second,
 /// and a `warn`ing on queries that take longer than 5 seconds.
 /// These thresholds will be configurable in a future version.
-pub struct LoggingConnection<C: Connection>(C);
+pub struct LoggingConnection<C: Connection> {
+    connection: Arc<C>,
+    transaction_manager: LoggingTransactionManager<C>,
+}
 
-impl<C: Connection> LoggingConnection<C> {
-    pub fn new(conn: C) -> Self {
-        LoggingConnection(conn)
+/// We are only wrapping SqliteConnection
+/// As we are using Arc this should be thread safe
+unsafe impl<C: Connection + Send> Send for LoggingConnection<C> {}
+
+impl<C: Connection + Send> LoggingConnection<C> {
+    pub fn new(connection: C) -> Self {
+        let connection = Arc::new(connection);
+        Self {
+            connection: connection.clone(),
+            transaction_manager: LoggingTransactionManager::new(connection),
+        }
     }
 }
 
-impl<C: Connection> Deref for LoggingConnection<C> {
-    type Target = C;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl<C> SimpleConnection for LoggingConnection<C>
+impl<C: Connection + Send> Connection for LoggingConnection<C>
 where
     C: Connection + Send + 'static,
-{
-    fn batch_execute(&self, query: &str) -> QueryResult<()> {
-        let start_time = Instant::now();
-        let result = self.0.batch_execute(query);
-        let duration = start_time.elapsed();
-        log_query(query, duration);
-        result
-    }
-}
-
-impl<C: Connection> Connection for LoggingConnection<C>
-where
-    C: Connection<TransactionManager = AnsiTransactionManager> + Send + 'static,
-    C::Backend: UsesAnsiSavepointSyntax,
     <C::Backend as Backend>::QueryBuilder: Default,
 {
     type Backend = C::Backend;
-    type TransactionManager = C::TransactionManager;
 
     fn establish(database_url: &str) -> ConnectionResult<Self> {
-        Ok(LoggingConnection(C::establish(database_url)?))
+        Ok(LoggingConnection::new(C::establish(database_url)?))
     }
 
     fn execute(&self, query: &str) -> QueryResult<usize> {
         let start_time = Instant::now();
-        let result = self.0.execute(query);
+        let result = self.connection.execute(query);
         let duration = start_time.elapsed();
         log_query(query, duration);
         result
     }
 
-    fn query_by_index<T, U>(&self, source: T) -> QueryResult<Vec<U>>
+    fn load<T, U, ST>(&self, source: T) -> QueryResult<Vec<U>>
     where
+        Self: Sized,
         T: AsQuery,
         T::Query: QueryFragment<Self::Backend> + QueryId,
-        Self::Backend: HasSqlType<T::SqlType>,
-        U: Queryable<T::SqlType, Self::Backend>,
+        T::SqlType: CompatibleType<U, Self::Backend, SqlType = ST>,
+        U: FromSqlRow<ST, Self::Backend>,
+        Self::Backend: QueryMetadata<T::SqlType>,
     {
         let query = source.as_query();
         let debug_query = debug_query::<Self::Backend, _>(&query).to_string();
         let start_time = Instant::now();
-        let result = self.0.query_by_index(query);
-        let duration = start_time.elapsed();
-        log_query(&debug_query, duration);
-        result
-    }
-
-    fn query_by_name<T, U>(&self, source: &T) -> QueryResult<Vec<U>>
-    where
-        T: QueryFragment<Self::Backend> + QueryId,
-        U: QueryableByName<Self::Backend>,
-    {
-        let debug_query = debug_query::<Self::Backend, _>(&source).to_string();
-        let start_time = Instant::now();
-        let result = self.0.query_by_name(source);
+        let result = self.connection.load(query);
         let duration = start_time.elapsed();
         log_query(&debug_query, duration);
         result
@@ -100,18 +80,84 @@ where
 
     fn execute_returning_count<T>(&self, source: &T) -> QueryResult<usize>
     where
+        Self: Sized,
         T: QueryFragment<Self::Backend> + QueryId,
     {
         let debug_query = debug_query::<Self::Backend, _>(&source).to_string();
         let start_time = Instant::now();
-        let result = self.0.execute_returning_count(source);
+        let result = self.connection.execute_returning_count(source);
         let duration = start_time.elapsed();
         log_query(&debug_query, duration);
         result
     }
 
-    fn transaction_manager(&self) -> &Self::TransactionManager {
-        self.0.transaction_manager()
+    fn transaction_manager(&self) -> &dyn TransactionManager<Self> {
+        &self.transaction_manager
+    }
+}
+
+impl<C> SimpleConnection for LoggingConnection<C>
+where
+    C: SimpleConnection + Connection + Send + 'static,
+{
+    fn batch_execute(&self, query: &str) -> QueryResult<()> {
+        let start_time = Instant::now();
+        let result = self.connection.batch_execute(query);
+        let duration = start_time.elapsed();
+        log_query(query, duration);
+        result
+    }
+}
+
+impl<C: 'static + Connection> R2D2Connection for LoggingConnection<C>
+where
+    C: R2D2Connection + Connection + Send + 'static,
+    <<C as Connection>::Backend as Backend>::QueryBuilder: Default,
+{
+    fn ping(&self) -> QueryResult<()> {
+        self.connection.ping()
+    }
+}
+
+struct LoggingTransactionManager<C: Connection> {
+    connection: Arc<C>,
+}
+
+impl<C: 'static + Connection> TransactionManager<LoggingConnection<C>>
+    for LoggingTransactionManager<C>
+where
+    <C::Backend as Backend>::QueryBuilder: std::default::Default,
+{
+    fn begin_transaction(&self, _conn: &LoggingConnection<C>) -> QueryResult<()> {
+        self.connection
+            .transaction_manager()
+            .begin_transaction(&self.connection)
+    }
+
+    fn rollback_transaction(&self, _conn: &LoggingConnection<C>) -> QueryResult<()> {
+        self.connection
+            .transaction_manager()
+            .rollback_transaction(&self.connection)
+    }
+
+    fn commit_transaction(&self, _conn: &LoggingConnection<C>) -> QueryResult<()> {
+        self.connection
+            .transaction_manager()
+            .commit_transaction(&self.connection)
+    }
+
+    fn get_transaction_depth(&self) -> u32 {
+        self.connection
+            .transaction_manager()
+            .get_transaction_depth()
+    }
+}
+
+impl<C: Connection> LoggingTransactionManager<C> {
+    pub fn new(conn: Arc<C>) -> Self {
+        Self {
+            connection: conn.clone(),
+        }
     }
 }
 
